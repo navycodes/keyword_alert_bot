fdf1d61f3e5c14c12d0e4ceb9eb699416996ef2d
Pang <hhtjim@gmail.com>
2025-06-07 01:32:50 +0800

    refactor: /list /subscribe output style

--- main.py
+++ main.py
@@ -601,7 +601,9 @@ async def subscribe(event):
   text = regex.sub(r'\s*,\s*',',',text) # 确保英文逗号间隔中间都没有空格  如 "https://t.me/xiaobaiup, https://t.me/com9ji"
   splitd = [i for i in regex.split(r'\s+',text) if i]# 删除空元素
   if len(splitd) <= 1:
-    await event.respond(r'输入需要订阅的关键字,支持js正则语法：`/[\s\S]*/ig`\n\nInput the keyword that needs to subscribe, support JS regular syntax：`/[\s\S]*/ig`')
+    msg = "输入需要订阅的关键字,支持js正则语法：\n`/[\s\S]*/ig`\n\nInput the keyword that needs to subscribe, support JS regular syntax：\n`/[\s\S]*/ig`"
+    text, entities = markdown.parse(msg)
+    await event.respond(text,formatting_entities=entities)
     cache.set('status_{}'.format(chat_id),{'current_status':'/subscribe keywords','record_value':text},expire=5*60)#设置5m后过期
   elif len(splitd)  == 3:
     command, keywords, channels = splitd
@@ -644,7 +646,8 @@ async def unsubscribe_all(event):
     msg = ''
     for keywords,channel_name,chat_id in _user_subscribe_list:
       channel_url = get_channel_url(channel_name,chat_id)
-      msg += 'keyword: {}\nchannel: {}\n---\n'.format(keywords,channel_url)
+      msg += f'Keyword: {keywords}\nChannel: {channel_url}\n{"---"*12}\n'
+
 
     re_update = utils.db.user_subscribe_list.update(status = 1 ).where(utils.User_subscribe_list.user_id == user_id)#更新状态
     re_update = re_update.execute()# 更新成功返回1，不管是否重复执行
@@ -908,7 +911,7 @@ async def _list(event):
             channel_username = f'channel username: {channel_name}\n'
 
         channel_url = f'<a href="{channel_url}-1">{"https://t.me/"+channel_name if channel_name else channel_url}</a>'
-        msg += f'id:{sub_id}\n{_type}: {keywords}\n{channel_title}{channel_username}channel url: {channel_url}\n---\n'
+        msg += f'ID: <code>{sub_id}</code>\n{_type.capitalize()}: <code>{keywords}</code>\nChannel: {" ".join([channel_title,channel_username,channel_url]).strip()}\n{"---"*12}\n'
       
       text, entities = html.parse(msg)# 解析超大文本 分批次发送 避免输出报错
       for text, entities in telethon_utils.split_text(text, entities):
